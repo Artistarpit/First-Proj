3c6e9f247b967d7229724a832dfa24020e2b6ab3
Artistarpit <arpittalmale4@gmail.com>
2024-03-12 12:50:24 +0530

    latest commit

---
+++ init_setup.sh
@@ -0,0 +1,9 @@
+echo [$(date)]: "START"
+echo [$(date)]: "creating env with python 3.8 version"
+conda create --prefix ./venv python=3.8 -y
+echo [$(date)]: "activating the environment"
+source activate ./venv
+
+echo [$(date)]: "installing the requirements"
+pip install -r requirements.txt
+echo [$(date)]: "END"   
